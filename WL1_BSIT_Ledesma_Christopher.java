a36f082e9eb401beb557bc8d921780040443818d
Haise781 <cjsamboledesma@gmail.com>
2025-10-22 00:11:50 +0800

    Add initial Java files for WL1 and WL2 exercises
    Added WL1_BSIT_Ledesma_Christopher.java with a loop printing a name 100 times. Created an empty WL2_BSIT_Ledesma_Christopher.java file as a placeholder for future implementation.

---
+++ WL1_BSIT_Ledesma_Christopher.java
@@ -0,0 +1,13 @@
+// Filename: WL1_BSIT_Ledesma_Christopher.java
+
+public class WL1_BSIT_Ledesma_Christopher {
+
+    public static void main(String[] args) {
+        int count = 1;
+
+        while (count <= 100) {
+            System.out.println(count + ". Christopher Ledesma");
+            count++;
+        }
+    }
+}
