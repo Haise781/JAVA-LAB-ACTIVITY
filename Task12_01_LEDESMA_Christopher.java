01fb6e92fe39f08c7d374b0891b0c34e6b480823
Haise781 <cjsamboledesma@gmail.com>
2025-11-06 14:01:10 +0800

    paractice task
    practice task

---
+++ Task12_01_LEDESMA_Christopher.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class Task12_01_LEDESMA_Christopher {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
+    
+}
