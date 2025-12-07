9dca327b16224ab235696e9222d508d420fc1a8b
Haise <cjsamboledesma@gmail.com>
2025-12-07 22:46:17 +0800

    afasf
    afsf

---
+++ task16a_Ledesma_Christopher.java
@@ -0,0 +1,13 @@
+public class task16a Ledesma_Christopher {
+    public static void main (String [] args) {
+
+        int[] numbers = {1, 5, 2, 5, 3, 5, 4};  // Array with multiple occurrences of the number 5
+        int count = 0;
+        for (int number : numbers) {  
+            if (number == 5) {
+                count++; // Incerement if the number is 5
+            }
+
+        }
+        System.out.println("The number 5 appears " + count + " times in the array."); // Print the output
+    }
---
+++ task16b Ledesma_Christopher.java
@@ -0,0 +1,9 @@
+public class task16b_Ledesma_Christopher {
+    public static void main (String [] args) {
+
+            int[] numbers = {10, 20, 30, 40, 50}; // Array of numbers
+            for (int i = numbers.length - 1; i >= 0; i--) {
+                System.out.println(numbers[i]); // Print output in reverse order
+            }
+        }
+    }
